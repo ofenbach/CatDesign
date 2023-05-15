403613fdfae53eaf41f3a0919c50af610b941f1a
ofenbach <t.ofenbach@web.de>
2023-05-15 11:08:19 +0200

    updated entire layout

--- CatDesign/main.py
+++ CatDesign/main.py
@@ -14,88 +14,123 @@ def main():
 
     cat = CatDesign()
 
-    # typography
-    cat.typography(text='Typography (in a box)', variant='h1', css='margin-top: 2em;', tailwind='ml-8')
-    with cat.box():
-        cat.typography(text='H1 Heading', variant='h1')
-        cat.typography(text='H2 Heading', variant='h2')
-        cat.typography(text='H3 Heading', variant='h3')
-        cat.typography(text='H4 Heading', variant='h4')
-        cat.typography(text='H5 Heading', variant='h5')
-        cat.typography(text='H6 Heading', variant='h6')
-        cat.typography(text='Subtitle 1', variant='subtitle1')
-        cat.typography(text='Subtitle 2', variant='subtitle2')
-        cat.typography(text='Body 1', variant='body1')
-        cat.typography(text='Body 2', variant='body2')
+    with cat.div(tailwind='ml-16 mr-16'):
 
-    cat.divider()
-    cat.card()
-    cat.divider()
+        # typography
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text='Typography (in a box)', variant='h1', css='margin-top: 2em;', tailwind='ml-8')
 
-    cat.typography(text='Notification', variant='h1')
-    with cat.div(tailwind='flex flex-wrap w-full justify-between'):
-        cat.notification()     # header_message and message param possible (string)
-        cat.notification()
-        cat.notification()
+        with cat.div(tailwind='flex w-full justify-center'):
+            with cat.box():
+                cat.typography(text='H1 Heading', variant='h1')
+                cat.typography(text='H2 Heading', variant='h2')
+                cat.typography(text='H3 Heading', variant='h3')
+                cat.typography(text='H4 Heading', variant='h4')
+                cat.typography(text='H5 Heading', variant='h5')
+                cat.typography(text='H6 Heading', variant='h6')
+                cat.typography(text='Subtitle 1', variant='subtitle1')
+                cat.typography(text='Subtitle 2', variant='subtitle2')
+                cat.typography(text='Body 1', variant='body1')
+                cat.typography(text='Body 2', variant='body2')
 
-    cat.divider()
+        cat.divider()
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text='Card', variant='h1')
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.card()
+        cat.divider()
 
-    cat.typography(text='Chip', variant='h1')
-    with cat.div(tailwind='flex flex-wrap w-full'):
-        cat.chip(variant='outlined')
-        cat.chip(variant='filled', css='margin-left: 64px;')
+        # notification
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text='Notification', variant='h1')
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.notification()     # header_message and message param possible (string)
 
-    cat.divider()
-    cat.typography(text='Rating', variant='h1')
-    cat.rating()
-    cat.divider()
+        cat.divider()
 
-    cat.typography(text='Alerts', variant='h1')
-    with cat.div(tailwind='flex flex-wrap w-full justify-between'):
-        cat.alert(type='info', message='This is an info alert — check it out!')
-        cat.alert(type='warning', message='This is a warning alert — check it out!')
-        cat.alert(type='error', message='Please enter correct e-mail')
-        cat.alert(type='success',  css="border-radius: 100px;", tailwind='mr-8')        # css and tailwind works
-    cat.divider()
+        # chip
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text='Chip', variant='h1')
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text="Chips are compact elements that represent an input, attribute, or action.",
+                           variant='body1')
+        with cat.div(tailwind='flex w-full justify-center mt-8'):
+            cat.chip(variant='outlined')
+            cat.chip(variant='filled', css='margin-left: 64px;')
 
-    cat.typography(text='Icons', variant='h1')
-    cat.typography(text='To use icons, use the icon method with the name of the icon and the desired variant.', variant='body1')
-    cat.typography(text='If no variant is specified, the default is "outlined".', variant='body1')
-    cat.typography(text='Example:', variant='body2')
-    cat.typography(text="cat.icon('share', variant='filled')", variant='body2')
-    cat.icon('share', variant='filled')
-    cat.typography(text="This will create a filled 'share' icon.", variant='body2')
-    cat.typography(text="You can also change the color of the icon.", variant='body2')
-    cat.typography(text="Example:", variant='body2')
-    cat.typography(text="cat.icon('share', color='red')", variant='body2')
-    cat.icon('share', color='red')
-    cat.typography(text="This will create a 'share' icon in red color.", variant='body2')
+        cat.divider()
 
 
-    cat.typography('List of all icons:')
-    with cat.div(tailwind='flex justify-between w-full'):
-        cat.icon('share')
-        cat.icon('like')
-        cat.icon('star')
-        cat.icon('settings')
-        cat.icon('hamburger')
-        cat.icon('refresh')
-        cat.icon('bell')
-        cat.icon('chevron-left')
-        cat.icon('chevron-right')
+        # rating
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text='Rating', variant='h1')
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text="Ratings provide insight regarding others' opinions and experiences, and can allow the user to submit a rating of their own.",
+                           variant='body1')
 
-    with cat.div(tailwind='flex justify-between w-full mt-4'):
-        cat.icon('cloud')
-        cat.icon('minus-circle')
-        cat.icon('ellipsis')
-        cat.icon('envelope')
-        cat.icon('envelope-open')
-        cat.icon('trash')
-        cat.icon('map')
-        cat.icon('map-pin')
-        cat.icon('hand-thumb-up')
+        with cat.div(tailwind='flex w-full justify-center mt-8'):
+            cat.rating()
 
-    cat.divider()
+        cat.divider()
+
+        # alerts
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text='Alerts', variant='h1')
+
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text="An alert displays a short, important message in a way that attracts the user's attention without interrupting the user's task.",
+                           variant='body1')
+        with cat.div(tailwind='flex flex-wrap w-full justify-between mt-8'):
+            cat.alert(type='info', message='This is an info alert — check it out!')
+            cat.alert(type='warning', message='This is a warning alert — check it out!')
+            cat.alert(type='error', message='Please enter correct e-mail')
+            cat.alert(type='success',  css="border-radius: 100px;", tailwind='mr-8')        # css and tailwind works
+
+        cat.divider()
+
+        # icons
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text='Icons', variant='h1')
+
+        with cat.div(tailwind='flex w-full justify-center'):
+            cat.typography(text='To incorporate icons, invoke the "icon" method, specifying the name of the icon as well as'
+                                'your preferred style variant. The icon names correspond to those found on the Heroicons'
+                                'website (https://heroicons.com/).', variant='body1')
+
+        """cat.typography(text='If no variant is specified, the default is "outlined".', variant='body1')
+        cat.typography(text='Example:', variant='body2')
+        cat.typography(text="cat.icon('share', variant='filled')", variant='body2')
+        cat.icon('share', variant='filled')
+        cat.typography(text="This will create a filled 'share' icon.", variant='body2')
+        cat.typography(text="You can also change the color of the icon.", variant='body2')
+        cat.typography(text="Example:", variant='body2')
+        cat.typography(text="cat.icon('share', color='red')", variant='body2')
+        cat.icon('share', color='red')
+        cat.typography(text="This will create a 'share' icon in red color.", variant='body2')"""
+
+        with cat.div(tailwind='flex justify-between w-full mt-8'):
+            cat.icon('share')
+            cat.icon('like')
+            cat.icon('star')
+            cat.icon('settings')
+            cat.icon('hamburger')
+            cat.icon('refresh')
+            cat.icon('bell')
+            cat.icon('chevron-left')
+            cat.icon('chevron-right')
+
+        with cat.div(tailwind='flex justify-between w-full mt-4'):
+            cat.icon('cloud')
+            cat.icon('minus-circle')
+            cat.icon('ellipsis')
+            cat.icon('envelope')
+            cat.icon('envelope-open')
+            cat.icon('trash')
+            cat.icon('map')
+            cat.icon('map-pin')
+            cat.icon('hand-thumb-up')
+
+        cat.divider()
 
 
     cat.tweet_card(profile_src='./images/cat0.png')
